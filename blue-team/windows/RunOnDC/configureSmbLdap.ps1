c4c786e6f0f181ec8e3d0f0e758675bb78ec5cf0
S1n1st3r <S1n1st3r@gmail.com>
2023-02-09 08:55:29 -0500

    Updates

--- blue-team/windows/RunOnDC/configureSmbLdap.ps1
+++ blue-team/windows/RunOnDC/configureSmbLdap.ps1
@@ -3,4 +3,6 @@ New-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name Restri
 New-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name RestrictAnonymousSAM -Value 1 -PropertyType DWORD -Force
 New-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name EveryoneIncludesAnonymous -Value 0 -PropertyType DWORD -Force 
 New-ItemProperty -Path "HKLM:\System\CurrentControlSet\Services\LanmanServer\Parameters" -Name RequireSecuritySignature -Value 1 -PropertyType DWORD -Force
+New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows NT\Reliability" -Name DontAddScheduledTasks -Value 1 -PropertyType DWORD -Force
+New-ItemProperty -Path "HKLM:\System\CurrentControlSet\Services\LanmanServer\Parameters" -Name SMB1 -Value 0 -PropertyType DWORD -Force
 Write-Host "Also run this script on any other Windows machines"
